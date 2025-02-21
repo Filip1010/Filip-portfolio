b10d40001992aecf856938fa349920eaf3a0d738
Filip Kuzmanoski <filipkuzmanoski04@gmail.com>
2025-02-21 22:44:59 +0100

    fix: correct spelling in Hero component and adjust animation speeds in InfiniteCards; update package dependencies

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Expesrience"
+            words="Transforming Concepts into Seamless User Experience"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
