77163673117b1129ec32248e6732f55fee148ced
Filip Kuzmanoski <filipkuzmanoski04@gmail.com>
2025-02-21 17:20:47 +0100

    ....

--- app/page.tsx
+++ app/page.tsx
@@ -15,15 +15,14 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        Test
-        {/* <FloatingNav navItems={navItems} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
-        <Footer /> */}
+        <Footer />
       </div>
     </main>
   );
